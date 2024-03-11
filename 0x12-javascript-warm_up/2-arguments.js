0b5c2075b8e2a10ad5a7315ed8725414925c1593
Wow77 <francmaki14@gmail.com>
2024-03-11 21:17:06 +0300

    2-arguments.js project edit

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-const readline = require ('readline');
 if (process.argv[2] === undefined) {
-	console.log('No argument');
+  console.log('No argument');
 } else {
-	console.log(process.argv[2]);
+  console.log(process.argv[2]);
 }
